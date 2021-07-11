d9e917fd8c312afd0c6c77401de4916391da3647
kelvin <76229164k@gmail.com>
2021-07-10 19:11:06 -0500

    camera

--- app/src/main/java/com/summer/camera/MainActivity.java
+++ app/src/main/java/com/summer/camera/MainActivity.java
@@ -2,13 +2,45 @@ package com.summer.camera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView intentCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intentCamera=findViewById(R.id.intentCamera);
+        intentCamera.setOnClickListener(callbackIntentCamera);
+
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String [] cameraId = cameraManager.getCameraIdList();
+            Log.d("testing","it works");
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
+
+    View.OnClickListener callbackIntentCamera=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    };
 }
